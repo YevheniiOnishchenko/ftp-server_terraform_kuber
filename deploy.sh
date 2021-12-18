b1ed7ac9078204e91b261cc0729fa053f84a7ec2
YevheniiOnishchenko <yevhenii.onishchenko.itis.2018@lpnu.ua>
2021-12-18 12:35:05 +0200

    add: congigMaps and StorageClass configuration

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+set -e
+
 terraform -chdir=terraform init
 terraform -chdir=terraform apply -auto-approve
 
 echo "$(terraform -chdir=terraform output -raw kube_config)" > ./terraform/connect.yml
 
 export KUBECONFIG=./terraform/connect.yml
+
+# Generate the ssh key pair
+ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key <<<n >/dev/null 2>&1
+ssh-keygen -t ed25519 -f ssh_host_ed25519_key <<<n >/dev/null 2>&1
+kubectl create secret generic ssh-privatekey --from-file ssh_host_rsa_key --from-file ssh_host_ed25519_key
 
 kubectl apply -f ./kubernetes
 
